c2927cd631bf1b4aaf4c9ecdfd75a7bd20ee4cb1
13runoo <brno20045@gmail.com>
2024-03-19 12:55:36 -0300

    pj

---
+++ resources/views/listaUsuarios.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../ListaUsuarios.css">
+<title>Lista de Usuários Cadastrados</title>
+</head>
+<body>
+<div class="container">
+    <h1>Lista de Usuários Cadastrados</h1>
+    <ul id="user-list">
+       
+    </ul>
+    <div>
+        <button class="btn" id="add-user-btn">Adicionar Usuário</button>
+        <button class="btn remove" id="remove-user-btn">Remover Usuário</button>
+    </div>
+</div>
+</body>
+</html>
